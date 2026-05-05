041ac4cc14c8d963e5c957a39201325a24a513dc
huy <sliverzack1@gmail.com>
2026-05-05 16:51:25 +0700

    chore(devops): add Docker local deployment and startup auto-migration

--- EzBias/EzBias.API/Program.cs
+++ EzBias/EzBias.API/Program.cs
@@ -75,6 +75,12 @@ builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<EzBiasDbContext>();
+    db.Database.Migrate();
+}
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
